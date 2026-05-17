4ea6d83e6828dbc5bd231c16d0454472065c7126
Md. Abul Hasan <abulhasan0292@gmail.com>
2026-05-18 00:26:52 +0600

    Delete courier_service directory

--- courier_service/src/app/core/models/notification.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Notification {
-  id?: number;
-  userId: number;
-  title: string;
-  message: string;
-  type: 'success' | 'info' | 'warning' | 'error';
-  read: boolean;
-  createdAt: string;
-}
--- courier_service/src/app/pages/parcel-list/parcel-list.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-parcel-list',
-  imports: [],
-  templateUrl: './parcel-list.html',
-  styleUrl: './parcel-list.css',
-})
-export class ParcelList {}
